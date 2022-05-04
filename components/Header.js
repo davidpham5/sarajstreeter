f2337108351b33cce67b6a2e73f53683b1fca418
David Pham <davidpham5@gmail.com>
2022-05-03 21:46:46 -0400

    change em dash

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,3 @@
 export default function Header({ title }) {
-  return <h1 className="text-3xl text-black mb-2">{title} <span className="">⸺</span></h1>
+  return <h1 className="text-3xl text-black mb-2">{title} <span className="font-thin">—</span></h1>
 }
