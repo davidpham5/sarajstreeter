de3620a7f0f617dd86a33a0f1e45f7364dc97259
David Pham <davidpham5@gmail.com>
2023-04-20 21:38:35 -0400

    move slash to inside loop

--- components/BoldList.js
+++ components/BoldList.js
@@ -3,12 +3,13 @@ export default function BoldList ({content}) {
     <div className="">
       <div className="w-full">
         <p className='font-bold text-3xl leading-60px tracking-one-and-half'>
-          /{" "}
+
           { // sort by created date
             content
               .sort((a, b) => new Date(b.publishedDate ) - new Date(a.publishedDate))
               .map((item, index) => (
                   <span key={item._id}>
+                    /{" "}
                     <a className='underline decoration-A73090 hover:underline decoration-indigo-700' href={item.url} target='blank'>{item.title}</a>
                     <span className='italic'> {item.publication}</span> / {item.publishedAt || item.publishedDate} {" "}
                   </span>
