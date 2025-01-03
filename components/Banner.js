ddee6385273db770e8690db3e66843402803e7cf
David Pham <davidpham5@gmail.com>
2025-01-03 00:27:51 -0500

    fix banner on mobile

--- components/Banner.js
+++ components/Banner.js
@@ -1,6 +1,6 @@
 export default function Banner({ title, link }) {
   return (
-    <div className="flex justify-center w-full bg-teal-200 ">
+    <div className="flex justify-center w-full bg-teal-200 pl-[30px]">
       <div className="flex">
         <h1 className="font-bold text-2xl tracking-one-and-half text-teal-800 py-4"><a href={link} target="_blank">{title}</a></h1>
       </div>
